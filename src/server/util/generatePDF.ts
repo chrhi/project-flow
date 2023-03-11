9c61732f063d0879192ee1c9a75d898a31b9744d
abdellah <Abdellah.chehri14@gmail.com>
2023-03-11 08:09:48 +0100

    add

---
+++ src/server/util/generatePDF.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import puppeteer  from 'puppeteer'
+import hb  from 'handlebars'
+
+
+
+
+export const generatePdf  = async (name:string , content:()=> string ) =>{
+
+const data = {};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+const template = hb?.compile(() => content(), { strict: true });
+
+// we have compile our code with handlebars
+const result = template(data);
+// We can use this to add dynamic data to our  template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
+const html = result;
+// we are using headless mode
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+const browser = await puppeteer.launch();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+const page = await browser.newPage()
+// We set the page content as the generated html by handlebars
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+await page.setContent(html)
+// We use pdf function to generate the pdf in the same folder as this file.
+//{ path: `./docs/${name}.pdf`, format: 'A4' }
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+const buffer = await (await page.pdf()).buffer
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+await browser.close();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+return buffer
+
+}
---
+++ src/server/util/templatesPdf/DetailsTemplates.ts
@@ -0,0 +1,134 @@
+
+export const DetailsTemplates = (
+    title : string , 
+    NeedForOrganization : string ,
+    ProjectRequirements : string ,
+    ProductDescription : string ,
+    ThePojectDoesNotInclude : string ,
+    PreApprovedResources : string 
+) => {
+
+
+    return `
+    <!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>project charte abdullah</title>
+        
+        <style>
+        
+        .pdf-box {
+            max-width: 800px;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #eee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+            font-size: 16px;
+            line-height: 24px;
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            color: #555;
+        }
+        .black-under__line{
+            width: 100%;
+            border-bottom: 5px solid black;
+        }
+        h1{
+            font-size: 2rem;
+            font-weight: 500;
+        }
+        h2{
+            font-size: 2.5rem;
+            font-weight: 600;
+        }
+        .row{
+            width: 100%;
+            height: 20px;
+            display: flex;
+            align-items: center;
+            padding: 1rem;
+        }
+        .textFiled__container{
+            width: 100%;
+            min-height: 50px;
+            display: flex;
+            flex-direction: column;
+            margin-top: 20px;
+    
+        }
+        .lable{
+            width: 100%;
+            height: 20px;
+            display: flex;
+            align-items: center;
+        }
+        
+        </style>
+    </head>
+    
+    <body>
+        <div class="pdf-box">
+            <div class="row">
+                <h1>Title de project : ${title} </h1>
+            </div>
+            <div class="row">
+                <h2 style="font-size: 2.2rem;">Charte du projet</h2>
+            </div>
+            <div class="row" style="margin-top: 1rem;">
+                <h1 style="font-size: 2.2rem; ">1- Details du projet</h1>
+            </div>
+            <div class="black-under__line"></div>
+    
+    
+            <div class="textFiled__container">
+               <div class="lable">
+                <h3>Besoin de l'organisation / objectifs du projet</h3>
+               </div>
+               <div>
+                <p>${NeedForOrganization}</p>
+               </div>
+            </div>
+    
+    
+            <div class="textFiled__container">
+                <div class="lable">
+                 <h3>Exigences du projet</h3>
+                </div>
+                <div>
+                 <p>${ProjectRequirements}</p>
+                </div>
+             </div>
+    
+    
+             <div class="textFiled__container">
+                <div class="lable">
+                 <h3>Description du produit / des livrables</h3>
+                </div>
+                <div>
+                 <p>${ProductDescription}</p>
+                </div>
+             </div>
+    
+             <div class="textFiled__container">
+                <div class="lable">
+                 <h3>Le projet n'inclut pas :</h3>
+                </div>
+                <div>
+                 <p>${ThePojectDoesNotInclude}</p>
+                </div>
+             </div>
+    
+    
+             <div class="textFiled__container">
+                <div class="lable">
+                 <h3>Ressources preapprouvees</h3>
+                </div>
+                <div>
+                 <p>${PreApprovedResources}</p>
+                </div>
+             </div>
+        </div>
+    </body>
+    </html>
+    `
+}
