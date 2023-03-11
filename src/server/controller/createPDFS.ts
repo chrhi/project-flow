6df34157b41a0dd344f5bb17635642ac19908be3
abdellah <Abdellah.chehri14@gmail.com>
2023-03-11 14:10:16 +0100

    add

--- src/server/controller/createPDFS.ts
+++ src/server/controller/createPDFS.ts
@@ -9,6 +9,14 @@ export const createPDFS = async  () => {
     .select('*')
     if(error ) throw new Error(error.message)
     if(projectDetails?.length === 0) throw new Error("there is no data")
+    const { data: ConsiderationsProject, error:error2 } = await supabase
+     .from('ConsiderationsProject')
+     .select('*')
+     if(error2 ) throw new Error(error2.message)
+     const { data: stakeholders, error:error3 } = await supabase
+     .from('stakeholders')
+     .select('*')
+     if(error3 ) throw new Error(error3.message)
     //after getting the data from the details table we assigned it to dataDetails vaiable
     const dataDetails : ParamsType = {
      title : projectDetails[0]?.titre as string  ,
@@ -16,12 +24,16 @@ export const createPDFS = async  () => {
      ProductDescription : projectDetails[0]?.ProductDescription as string ,
      PreApprovedResources : projectDetails[0]?.PreApprovedResources as string ,
      ProjectRequirements :  projectDetails[0]?.ProjectRequirements as string ,
-     ThePojectDoesNotInclude : projectDetails[0]?.ThePojectDoesNotInclude as string 
+     ThePojectDoesNotInclude : projectDetails[0]?.ThePojectDoesNotInclude as string ,
+     Hypotheses : ConsiderationsProject[0]?.Hypotheses as string ,
+     AcceptanceCriteria : ConsiderationsProject[0]?.AcceptanceCriteria as string ,
+     Constraints : ConsiderationsProject[0]?.Constraints as string ,
+     HighLevelRisks : ConsiderationsProject[0]?.HighLevelRisks as string ,
+     stakeHolders : stakeholders
+     
     }
 
-    console.log("this is from the pdf data project")
-    console.log(dataDetails)
-    console.log(projectDetails)
+  
     //we get the buffer from the template and the data
     const PdfAsString = PdfTemplates(dataDetails)
     const buffer = await generatePdf(PdfAsString )
