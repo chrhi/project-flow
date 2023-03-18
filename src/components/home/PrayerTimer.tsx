2cd7b1532d88d120ca5b7c0afacbdc7eba13fdd3
abdellah <Abdellah.chehri14@gmail.com>
2023-03-18 17:23:29 +0100

    add

--- src/components/home/PrayerTimer.tsx
+++ src/components/home/PrayerTimer.tsx
@@ -17,31 +17,22 @@ export default function PrayerTimes() {
     };
   }, []);
 
+  const BlockClock = (name:string , showBorder = true) => (
+    <div className={`w-[18%] h-[80%] ${showBorder ? "border-r border-gray-600 " : ""} flex flex-col items-center justify-center gap-y-4 `}>
+    <Clock renderSecondHand={false} value={value} className={style.clock} />
+    <h1>{name}</h1>
+    </div>
+  )
 
 
   return (
 
     <div className='w-[60%]  h-[200px] flex gap-x-2 bg-white rounded-lg items-center justify-center' >
-      <div className={`w-[18%] h-[80%]  flex flex-col items-center justify-center gap-y-4 `}>
-           <Clock  value={value} className={style.clock} />
-           <h1>abdullah</h1>
-       </div>
-       <div className={`w-[18%] h-[80%]  flex flex-col items-center justify-center gap-y-4 `}>
-           <Clock  value={value} className={style.clock} />
-           <h1>abdullah</h1>
-       </div>
-       <div className={`w-[18%] h-[80%]  flex flex-col items-center justify-center gap-y-4 `}>
-           <Clock  value={value} className={style.clock} />
-           <h1>abdullah</h1>
-       </div>
-       <div className={`w-[18%] h-[80%]  flex flex-col items-center justify-center gap-y-4 `}>
-           <Clock  value={value} className={style.clock} />
-           <h1>abdullah</h1>
-       </div>
-       <div className={`w-[18%] h-[80%]  flex flex-col items-center justify-center gap-y-4 `}>
-           <Clock  value={value} className={style.clock} />
-           <h1>abdullah</h1>
-       </div>
+     {BlockClock("fadjr" )}
+     {BlockClock("dohr")}
+     {BlockClock("assar")}
+     {BlockClock("makhrib")}
+     {BlockClock("ishaaa", false)}
     </div>
 
     
