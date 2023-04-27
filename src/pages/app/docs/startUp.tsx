1a6fde5a08d44cbdda01876357aea47e27686275
abdellah <Abdellah.chehri14@gmail.com>
2023-04-27 20:31:03 +0200

    add

--- src/pages/app/docs/startUp.tsx
+++ src/pages/app/docs/startUp.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 import { type NextPage } from "next";
 import Head from "next/head";
 import {  useState } from "react";
 import { Header } from "~/components/common/Header";
 import { FolderIcon } from "@heroicons/react/20/solid";
-import { useRouter } from "next/router";
+import FileDocs from "~/components/docsComponents/file";
+
 
 const LISTA = [
   {
@@ -32,8 +32,6 @@ const LISTA = [
 
 const Page: NextPage = () => {
   const [isOpen , setIsOpen] = useState<boolean>(true)
-
-  const router = useRouter()
  
   return (
     <>
@@ -47,20 +45,16 @@ const Page: NextPage = () => {
       </Head>
       <Header />
       <main className=" custopn-page-height  flex w-full justify-center items-center bg-gray-50 ">
-      <div className="w-[80%] h-[80%] rounded-lg flex gap-x-4 bg-white shadow-lg p-4 flex-wrap ">
-
-        {
-          LISTA.map(item => (
-            <div key={item.name} 
-            onClick={() => router.push("/app/docs/startUp") }
-            className="w-[100px] h-[100px] hover:bg-blue-100 cursor-pointer rounded-lg flex flex-col items-center p-4">
-            <FolderIcon className="h-24 w-24 text-blue-300  " />
-            <p className="text-sm text-gray-800 font-medium leading-3">{item.name}</p>
-          </div>
-          ))
-        }
-
-
+      <div className="w-[80%] h-[80%] rounded-lg flex gap-x-4 bg-white shadow-lg  flex-col ">
+        <div className="w-full h-[70px] flex justify-start items-center pl-4">
+                <button>
+                    return 
+                </button>
+        </div>
+        <div className="w-full min-h-[300px] h-full flex gap-x-4 p-8 ">
+        <FileDocs isDocument= {false} text="project charter" />
+        <FileDocs isDocument= {false} text="bla bla" />
+        </div>
       </div>
       </main>
     </>
