d45da29df45f8ea0bbb00b0a2fb0471b64fe8df0
abdellah <Abdellah.chehri14@gmail.com>
2023-05-26 21:52:08 +0200

    fix bug

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,3 @@
-/**
- * This is the client-side entrypoint for your tRPC API. It is used to create the `api` object which
- * contains the Next.js App-wrapper, as well as your type-safe React Query hooks.
- *
- * We also create a few inference helpers for input and output types.
- */
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
