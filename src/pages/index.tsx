ef1f9288961da937fc3c93f1e78d0aa679a69d35
abdellah <Abdellah.chehri14@gmail.com>
2023-05-29 01:47:15 +0200

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
 import { type NextPage } from "next";
-import Background from "~/components/common/background";
-import Hero from "~/components/landing-page/Hero";
-import Nav from "~/components/landing-page/Nav";
-import Demo from "~/components/landing-page/demo";
-import GlobeClient from "~/components/landing-page/glob-client";
-import Logos from "~/components/landing-page/logs";
-import Stats from "~/components/landing-page/stats";
+
 import Head from "~/components/common/Head";
 
 const Page: NextPage = () => {
@@ -15,14 +9,9 @@ const Page: NextPage = () => {
     <>
       <Head />
        <div className="z-10 bg-white">
-        <Nav />
-        <Hero  />
-        <Demo />
-        <Logos />
-        <GlobeClient markers={[]}/>
-        <Stats />
+        <h1>this is </h1>
        </div>
-       <Background />
+      
       </>
   );
 };
