2f67aa832eef41c1a1ce697fdc4f3d96be392cd2
abdellah <Abdellah.chehri14@gmail.com>
2023-07-19 07:58:22 +0200

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,3 +7,5 @@ export const config = { matcher: ["/app"] }
 
 //   //todo handle that later
 // }
+
+//https://docs.google.com/forms/d/e/1FAIpQLSdtuFf-X6DBaC94dF4oNCHjI1QIhkSz8GRxRAC2T-ApmDovCw/viewform
