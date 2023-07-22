4a209843c0ef93f5ddf13cbad02a329b39e0942d
abdellah <Abdellah.chehri14@gmail.com>
2023-07-22 10:03:44 +0200

    add

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -61,8 +61,8 @@ const Messages: FC<MessagesProps> = ({
 
 
   function getFormattedHourAndMinutesFromDate(date: Date): string {
-    const hour = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const hour = date?.getHours()?.toString()?.padStart(2, '0');
+    const minutes = date?.getMinutes()?.toString()?.padStart(2, '0');
   
     return `${hour}:${minutes}`;
   }
