2b3276af8c9995c91a8ec7edee92185633f3a211
abdellah <Abdellah.chehri14@gmail.com>
2023-07-23 00:34:49 +0200

    add

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -6,7 +6,7 @@ import PusherClient from 'pusher-js';
 
 export const pusherServer = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_APP_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: 'eu',
   useTLS: true,
